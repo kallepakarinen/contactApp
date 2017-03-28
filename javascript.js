56aec3550fed14e359fcd75669ad99959d0f5248
Kalle Pakarinen <cuttu85@gmail.com>
2017-03-28 14:34:16 +0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
 /**
  * Created by ekoodi on 14.3.2017.
  */
+function loadPage() {
+    document.getElementById("myTable").innerHTML = tableFill();
+}
+
 ///send inputs -> contact.js
 function ContactInput(){
     return contactsApp.contact(
@@ -8,9 +12,16 @@ function ContactInput(){
         document.getElementById('input-ln').value,
         document.getElementById('input-phone').value,
         document.getElementById('input-address').value,
-     document.getElementById('input-city').value
-
+        document.getElementById('input-city').value
     );
+}
+
+function inputResetBubbleGum(){                                                 ///that must be better way
+    document.getElementById("input-fn").value = "";
+    document.getElementById("input-ln").value = "";
+    document.getElementById("input-phone").value = "";
+    document.getElementById("input-address").value = "";
+    document.getElementById("input-city").value = "";
 }
 
 function tableFill() {
@@ -22,7 +33,7 @@ function tableFill() {
     var tableMid = "</td><td>";
     var tableEnd = "</td></tr>";
    var googleMap = "<p><a target='_blank' href='http://maps.google.com/?q=";
-    var tableHeader = tableStart + "Firstname" + tableMid + "Lastname" + tableMid +  "Phone" + tableMid + "Address" + tableEnd;
+    var tableHeader = tableStart + "Firstname" + tableMid + "Lastname" + tableMid +  "Phone" + tableMid + "Address" + tableMid + "" + tableMid + "" + tableEnd;
     for (i = 0; i < localStorageObjectLength; i++) {
         var fullAddress = person[i].address + " " + person[i].city;
      var tableDeleteButton = "<button type='button' id=" + i + "; onclick='deleteTableButton(this.id)' class='mdl-button mdl-js-button mdl-button--raised'>Delete</button>";
@@ -32,49 +43,60 @@ function tableFill() {
     return tableHeader + fillTable;
 }
 
-function onLoadPage() {
-    document.getElementById("myTable").innerHTML = tableFill();
+function deleteTableButton(clicked_id) {
+    if (confirm('Are you sure you want to delete this contact?')) {
+        var persons = JSON.parse(localStorage.getItem("contacts"));
+        var x = parseInt(clicked_id);
+        var index = persons.indexOf(persons[x]);
+        if (index > -1) {
+            persons.splice(index, 1);
+        }
+        localStorage.setItem("contacts", JSON.stringify(persons));
+     loadPage();
+    }
 }
 
-function deleteTableButton(clicked_id) {
-    var persons = JSON.parse(localStorage.getItem("contacts"));
-    var x = parseInt(clicked_id);
-    var index = persons.indexOf(persons[x]);
-    if (index > -1) {
-        persons.splice(index, 1);
-    }
-    localStorage.setItem("contacts", JSON.stringify(persons));
-    document.getElementById("myTable").innerHTML = tableFill();
+function checkIfInputEmpty(){
+   if(ContactInput().firstName == "" || ContactInput().lastName == "" || ContactInput().address == "" || ContactInput().city == ""){
+       return true;
+   }
 }
 
 function updateTableButton(clicked_id) {
-    var x = parseInt(clicked_id);
-    var persons = JSON.parse(localStorage.getItem("contacts"));
-    var index = persons.indexOf(persons[x]);
-    if (ContactInput().firstName == "" || ContactInput().lastName == "" || ContactInput().address == "" || ContactInput().city == ""){
-        alert("Fill all textfields");
-
+    if (confirm('Are you sure you want to update this contact?')) {
+        var x = parseInt(clicked_id);
+        var persons = JSON.parse(localStorage.getItem("contacts"));
+        var index = persons.indexOf(persons[x]);
+        if (checkIfInputEmpty() == true) {
+            alert("Fill all textfields");
+        }
+        else {
+            if (index > -1) {
+                persons[x] = ContactInput();
+            }
+            localStorage.setItem("contacts", JSON.stringify(persons));
+            inputResetBubbleGum();
+        }
+        loadPage();
     }
-    else{
-    if (index > -1) {
-     persons[x] = ContactInput();
-    }
-    }
-    localStorage.setItem("contacts", JSON.stringify(persons));
-    document.getElementById("myTable").innerHTML = tableFill();
-
 }
 
 function saveButton() {
     var person = JSON.parse(localStorage.getItem("contacts"));
+if(checkIfInputEmpty() !== true) {
     if (typeof person !== 'undefined' && person !== null) {
-         person.push(ContactInput());
+        person.push(ContactInput());
         localStorage.setItem("contacts", JSON.stringify(person));
-    }else{
+    } else {
         contacts.push(ContactInput());
         localStorage.setItem("contacts", JSON.stringify(contacts));
     }
-    document.getElementById("myTable").innerHTML = tableFill();
+    alert("1 contact added");
+    inputResetBubbleGum();
+    loadPage();
+}else{
+    alert("Fill all textfields");
+}
 }
 
 /**
